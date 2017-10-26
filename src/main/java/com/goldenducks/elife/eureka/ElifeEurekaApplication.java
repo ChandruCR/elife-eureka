937f2d3605dedf9c05b52a88ffd0e68aaa0027a7
Chandru <mailchandrucr@gmail.com>
2017-10-26 02:42:14 +0200

    Final Changes 26102017

--- src/main/java/com/goldenducks/elife/eureka/ElifeEurekaApplication.java
+++ src/main/java/com/goldenducks/elife/eureka/ElifeEurekaApplication.java
@@ -1,5 +1,7 @@
-package com.goldenducks.elifeeureka;
+package com.goldenducks.elife.eureka;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
@@ -8,7 +10,10 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @SpringBootApplication
 public class ElifeEurekaApplication {
 
+	private static final Logger logger = LoggerFactory.getLogger(ElifeEurekaApplication.class);
+	
 	public static void main(String[] args) {
+		logger.debug("Starting eLifeEureka Service Registry and Discovery");
 		SpringApplication.run(ElifeEurekaApplication.class, args);
 	}
 }
